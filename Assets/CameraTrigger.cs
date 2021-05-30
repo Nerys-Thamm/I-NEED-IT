d3d0ea3ee8c9d14610100f192e6312bba7f6c41a
Nerys <NerysThamm@gmail.com>
2021-05-31 01:57:24 +1200

    Prototyped camera stuff

---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,36 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public CinemachineVirtualCamera Camera;
+    
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Camera.MoveToTopOfPrioritySubqueue();
+            //input.SwitchCurrentActionMap(actionmapname);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 1, 0.5f);
+        Gizmos.DrawCube(transform.position, this.gameObject.GetComponent<BoxCollider>().bounds.extents*2);
+           
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+}
---
+++ Assets/Movement_Normal.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement_Normal : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float speedmult;
+
+    public Transform Camera;
+
+    Vector2 moveVal;
+
+    void OnMove(InputValue value)
+    {
+        moveVal = value.Get<Vector2>();
+    }
+
+    void OnJump()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var forward = Camera.forward;
+        var right = Camera.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        var direction = forward * moveVal.y + right * moveVal.x;
+
+        transform.Translate(direction * Time.deltaTime * speedmult);
+    }
+}
