4e29ef959e22c888f1c40ad06bdea3ca21392f1f
Nerys <NerysThamm@gmail.com>
2021-06-09 22:20:01 +1200

    Made platforms Sticky

--- Assets/Environment/MovingObjectPath/PathMovementAgent.cs
+++ Assets/Environment/MovingObjectPath/PathMovementAgent.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PathMovementAgent : MonoBehaviour
 {
     public MovementPath m_Path;
+    public UnityEvent m_OnNodeReach;
     public float m_Speed;
     public float m_distancetonextnode;
     Vector3 m_direction;
@@ -31,6 +33,7 @@ public class PathMovementAgent : MonoBehaviour
             m_nextnode = m_Path.GetNextNode();
             m_direction = (m_nextnode.Position - this.gameObject.transform.position);
             m_distancetonextnode = m_direction.magnitude;
+            m_OnNodeReach.Invoke();
         }
         this.gameObject.transform.position += m_direction.normalized * (m_Speed * m_nextnode.SpeedCurve.Evaluate(1 / m_distancetonextnode)) * Time.deltaTime;
     }
