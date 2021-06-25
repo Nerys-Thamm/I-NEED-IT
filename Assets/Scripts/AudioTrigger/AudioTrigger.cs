88d73eab989ec531b0e5637512ea2eebd95d10a2
Nerys <NerysThamm@gmail.com>
2021-06-25 21:34:13 +1200

    Audio trigger added FUCKFUCKFUCKFUCKFUCKFUCKSHITFUCKFUCKKILLME

---
+++ Assets/Scripts/AudioTrigger/AudioTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public UnityEvent m_OnTrigger;
+
+    bool m_CanBeTriggered = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && m_CanBeTriggered)
+        {
+            m_OnTrigger.Invoke();
+            m_CanBeTriggered = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(this.transform.position, GetComponent<BoxCollider>().extents);
+    }
+}
