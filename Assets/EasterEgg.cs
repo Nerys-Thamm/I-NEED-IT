e7a0f8f2b11029baca3d6063c7eb5b35e8d6e533
Nerys <NerysThamm@gmail.com>
2021-06-28 02:11:33 +1200

    Added in a little surprise for some ~Sussy~ bakas

--- Assets/EasterEgg.cs
+++ Assets/EasterEgg.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EasterEgg : MonoBehaviour
 {
     public AudioSource m_player;
+    public AudioClip m_clip;
     public GameObject m_model;
     public bool m_isenabled;
     // Start is called before the first frame update
@@ -24,11 +25,14 @@ public class EasterEgg : MonoBehaviour
         if (m_isenabled)
         {
             m_player.Stop();
+            m_player.volume = 1.0f;
             m_model.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
             m_isenabled = false;
         }
         else
         {
+            m_player.clip = m_clip;
+            m_player.volume = 0.1f;
             m_player.Play();
             m_model.transform.localScale = new Vector3(2.0f, 0.7f, 0.7f);
             m_isenabled = true;
