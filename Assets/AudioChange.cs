1044046a0b429805472d0d416411ab5456e7191f
Stef <theepicstef@users.noreply.github.com>
2021-06-29 00:46:24 +1200

    Changes ambience in level 3 when entering new room

---
+++ Assets/AudioChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioChange : MonoBehaviour
+{
+
+    public AudioSource Source;
+    public AudioClip TrackToSwapTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            Source.clip = TrackToSwapTo;
+            Source.Play();
+        }
+    }
+}
