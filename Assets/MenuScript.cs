f347cf7840027db93061aafa022cf6c1e2fc9030
Stef <theepicstef@users.noreply.github.com>
2021-06-28 22:15:19 +1200

    Menu Works

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject MenuStuff;
+    public Animator PlayerAnimator;
+    public GameObject AudioTrigger;
+    public GameObject canvas;
+    public PlayerInput Input;
+
+    private void Awake()
+    { 
+        canvas.SetActive(true);
+        PlayerAnimator.speed = 0.0f;
+        Input.SwitchCurrentActionMap("MainMenu");
+    }
+
+    public void OnPlay()
+    {
+        Debug.Log("AAA");
+        PlayerAnimator.speed = 1.0f;
+        AudioTrigger.SetActive(true);
+        MenuStuff.SetActive(false);
+        Input.SwitchCurrentActionMap("Movement");
+    }
+
+    private void OnQuit()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
